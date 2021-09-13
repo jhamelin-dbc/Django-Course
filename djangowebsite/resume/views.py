825228a26e63724485e79d1f7bffc70fbf14830a
Jonathan Hamelin <jhamelin@dbc-labs.com>
2021-09-13 13:38:10 -0400

    removed about page

--- djangowebsite/resume/views.py
+++ djangowebsite/resume/views.py
@@ -3,8 +3,3 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html', {})
-
-def about(request):
-	return render(request, 'about.html', dict_1)
-
-dict_1 = {'first_name':'Jonathan', 'last_name':'Hamelin'}
